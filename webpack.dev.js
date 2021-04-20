6eef615229308b21a6b0275b3894dad759a4734c
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-04-20 04:52:36 +0200

    Don't make webpack verbose

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,6 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
@@ -26,7 +25,7 @@ module.exports = {
             // Simulate the removal of files
             dry: true,
             // Write Logs to Console
-            verbose: true,
+            verbose: false,
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
